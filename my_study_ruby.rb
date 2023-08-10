e332dbf4bd3d882dc4692328c76a95d8ac7291b3
Rogerio Zeferino <rogeriozeferino1981@gmail.com>
2023-08-10 15:18:04 -0300

    Criação do arquivo calc e atualização do arquivo my_study_ruby

--- my_study_ruby.rb
+++ my_study_ruby.rb
@@ -1,1 +1,33 @@
-puts "Hello World"
+#puts "Hello World"#
+
+
+#print 'Digite seu nome:  '
+
+#name = gets.chomp
+
+#puts "Hello #{name}!" 
+
+
+
+#print 'Digite seu nome:  '
+
+#name = gets.chomp
+
+#print 'Digite seu sobrenome:  '
+
+#sobrenome = gets.chomp
+
+#puts "Hello #{name} #{sobrenome}!"
+
+
+
+
+
+
+
+
+
+
+
+
+
